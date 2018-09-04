6976700fe2dbc3e7d771b8fa5d36d40ac249d9c6
irina <irina.gavenko@phystech.edu>
2018-09-04 20:24:22 +0300

    first version with errors

--- src/task_schedule/core.clj
+++ src/task_schedule/core.clj
@@ -1,2 +1,47 @@
 (ns task-schedule.core)
 
+(def task-list [{:task-type "hello"
+                 :schedule 5000
+                 :params [1]}
+                {:task-type "hello2"
+                 :schedule 10000
+                 :params [2 3]}
+                {:task-type "hello3"
+                 :schedule 12000
+                 :params [4 5 6]}])
+
+(defn hello-processing
+  [values]
+  (println values))
+
+(defn hello2-processing
+  [values]
+  (println values))
+
+(defn hello3-processing
+  [values]
+  (println values))
+
+#_(def task {:task-type "hello2"
+           :schedule 5000
+           :params [1 2]})
+
+(def stop-value 1)
+
+(defn next-task
+  [task]
+  (println (task :task-type))
+  (case (task :task-type)
+    "hello" (hello-processing (task :params))
+    "hello2" (hello-processing (task :params))
+    "hello3" (hello-processing (task :params)))
+  ((Thread/sleep (task :schedule))))
+
+(defn run-task
+  [task-list]
+  (while (< stop-value 2) (do
+                           (map (fn [task'] (next-task task')) task-list)
+                           (println stop-value)
+                           (def stop-value (inc stop-value)))))
+
+(run-task task-list)
