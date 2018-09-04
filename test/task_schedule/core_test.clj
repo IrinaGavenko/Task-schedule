ca6c521ad00a0f6c6e7eff49c1be3cc1be734310
irina <irina.gavenko@phystech.edu>
2018-09-04 18:56:27 +0300

    init

---
+++ test/task_schedule/core_test.clj
@@ -0,0 +1,7 @@
+(ns task-schedule.core-test
+  (:require [clojure.test :refer :all]
+            [task-schedule.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
