5a48e1b6174f12fcb7b38d124b50dd04575d9305
irina <irina.gavenko@phystech.edu>
2018-09-05 23:35:57 +0300

    baseline

--- src/task_schedule/core.clj
+++ src/task_schedule/core.clj
@@ -3,16 +3,16 @@
 ;; Add parameters of new task
 
 (def task-list [{:task-type "task1"
-                 :schedule 5000
+                 :schedule 5
                  :params [1]}
                 {:task-type "task2"
-                 :schedule 10000
+                 :schedule 10
                  :params [2 3]}
                 {:task-type "task3"
-                 :schedule 12000
+                 :schedule 12
                  :params [4 5 6]}
                 {:task-type "task4"
-                 :schedule 15000
+                 :schedule 15
                  :params [7 8 9 10]}])
 
 ;; Add execution function of new task
@@ -41,31 +41,54 @@
            :schedule 5000
            :params [1 2 3]})
 
-;; Add task-type to controller-function
-
-(defn new-task
-  "Find and launch processing of new task"
-  [{:keys [task-type schedule params]}]
-    (case task-type
-      "task1" (task1-processing params)
-      "task2" (task2-processing params)
-      "task3" (task3-processing params)
-      "task4" (task4-processing params))
-    (Thread/sleep schedule))
 
 #_(new-task task)
-
-;; run application
-
-(defn run
-  "Main function -- run application"
-  [task-list]
-  (run! new-task task-list))
 
 #_(defn run
   "Main function -- run application"
   [task-list]
-  (while true?
-    (run! #(new-task %1) task-list)))
+  (run! new-task task-list))
+
+(def curr-time 13)
+(def task-list' [])
+
+(defn add-task
+  [task]
+  (def task-list' (conj task-list' task)))
+
+(defn launch-task
+  "Find and launch processing of new task"
+  [{:keys [task-type schedule params]}]
+  (case task-type
+    "task1" (task1-processing params)
+    "task2" (task2-processing params)
+    "task3" (task3-processing params)
+    "task4" (task4-processing params))
+  (def task {:task-type task-type
+             :schedule (+ 1 schedule)
+             :params params})
+  (add-task task))
+
+;; Максимально упрощенная версия
+(defn check-time
+  "Necessity to run a task"
+  [schedule]
+  (if (<= schedule curr-time)
+    true
+    false))
+
+(defn next-task
+  [{:keys [task-type schedule params] :as task}]
+  (if (check-time schedule)
+    (launch-task task)
+    (add-task task)))
+
+;; run application
+
+(defn run
+  [task-list]
+  (def task-list' [])
+  (run! next-task task-list)
+  (recur task-list'))
 
 (run task-list)
